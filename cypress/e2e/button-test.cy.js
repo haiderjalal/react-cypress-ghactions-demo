821ef1050cb6544103469643650724e98f4fa79a
haiderjalal <haiderjalal1555@gmail.com>
2025-01-14 13:40:46 +0500

    testcase updated for second button

--- cypress/e2e/button-test.cy.js
+++ cypress/e2e/button-test.cy.js
@@ -1,7 +1,13 @@
 describe('React App Button Click Test', () => {
   it('should display message when button is clicked', () => {
-    cy.visit('http://localhost:3000'); // Adjust the URL if different
+    cy.visit('http://localhost:3000'); 
     cy.contains('Click Me').click();
     cy.contains('Button clicked!').should('be.visible');
+  });
+
+  it('should display message when the second button is clicked', () => {
+    cy.visit('http://localhost:3000');
+    cy.contains('Click Me Too').click();
+    cy.contains('Second button clicked!').should('be.visible');
   });
 });
