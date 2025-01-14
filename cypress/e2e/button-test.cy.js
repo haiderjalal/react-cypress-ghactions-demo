784e7784df2fb64be6b53d83db20b501678c8dbf
haiderjalal <haiderjalal1555@gmail.com>
2025-01-14 18:05:39 +0500

    TC 2 error message changed should pass the test case

--- cypress/e2e/button-test.cy.js
+++ cypress/e2e/button-test.cy.js
@@ -8,6 +8,6 @@ describe('React App Button Click Test', () => {
   it('should display message when the second button is clicked', () => {
     cy.visit('http://localhost:3000');
     cy.contains('Click Me Too').click();
-    cy.contains('should give an error').should('be.visible');
+    cy.contains('Second button clicked!').should('be.visible');
   });
 });
